57325ab21763610a1360bcaa8ebc20fd9b273f0a
streeg <guilhermeandreuce@gmail.com>
2017-05-30 14:30:14 -0300

    Blog atualizado e implementado até o tópico 5.10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
     root 'welcome#index'
     get 'welcome/index' => 'welcome#index'
+    get 'articles/new' => 'articles#new', as: :new_article
+    get 'articles/:id'=> 'articles#show', as: :article
+    post 'articles' => 'articles#create', as:  :articles
+    get 'articles' => 'articles#index'
+
 end
