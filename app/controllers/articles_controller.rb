51a92c05be85928da0c954d6c7701f611e059eb8
streeg <guilhermeandreuce@gmail.com>
2017-06-14 12:02:57 -0300

    versão final do blog

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,52 @@
 class ArticlesController < ApplicationController
-    def new
-        @article = Article.new
+
+  http_basic_authenticate_with name: "gui", password: "batata", except: [:index, :show]
+
+  def new
+    @article = Article.new
+  end
+
+  def index
+    @article = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
-    def index
-        @article = Article.all
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
+  end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
 
-    def show
-        @article = Article.find(params[:id])
-    end
+    redirect_to articles_path
+  end
 
-    def create
-        @article = Article.new(article_params)
-
-        if @article.save
-            redirect_to @article
-        else
-            render 'new'
-        end
-    end
-
-    private
-    def article_params
-        params.require(:article).permit(:title, :text)
-    end
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
     root 'welcome#index'
-    get 'welcome/index' => 'welcome#index'
-    get 'articles/new' => 'articles#new', as: :new_article
-    get 'articles/:id'=> 'articles#show', as: :article
-    post 'articles' => 'articles#create', as:  :articles
-    get 'articles' => 'articles#index'
+    resources :articles do
+        resources :comments
+    end
+   get 'welcome/index' => 'welcome#index'
+    #get 'articles/new' => 'articles#new', as: :new_article
+    #get 'articles/:id'=> 'articles#show', as: :article
+    #post 'articles' => 'articles#create', as:  :articles
+    #get 'articles' => 'articles#index'
+    #get 'articles/:id/edit' => 'articles#edit', as: :edit_article
+    #patch 'articles/:id' => 'articles#update'
 
 end
